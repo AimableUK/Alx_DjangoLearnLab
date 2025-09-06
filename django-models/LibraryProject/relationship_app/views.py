1767e984e36dc0aeaf1242e7e24329f00d112c47
UKOBIZABA Aimable <154277022+AimableTrague@users.noreply.github.com>
2025-09-06 09:22:40 +0200

    Auth Setup

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library  # checker wants this line
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.decorators import login_required
 
 
 # Function-Based View (checker expects this exact style)
@@ -17,3 +21,35 @@ class LibraryDetailView(DetailView):
 
     def get_object(self, queryset=None):
         return get_object_or_404(Library, id=self.kwargs["pk"])
+
+
+
+# Login view
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("book_list")  # redirect after login
+    else:
+        form = AuthenticationForm()
+    return render(request, "relationship_app/login.html", {"form": form})
+
+# Logout view
+@login_required
+def logout_view(request):
+    logout(request)
+    return render(request, "relationship_app/logout.html")
+
+# Registration view
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # login immediately after registration
+            return redirect("book_list")
+    else:
+        form = UserCreationForm()
+    return render(request, "relationship_app/register.html", {"form": form})
