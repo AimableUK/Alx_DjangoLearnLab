ac7802a862e8436e277143e31e76f7f4667b1a92
UKOBIZABA Aimable <154277022+AimableTrague@users.noreply.github.com>
2025-09-06 09:04:19 +0200

    Library Import Implementation

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Book
+from .models import Library  # checker wants this line
+
 
 # Function-Based View (checker expects this exact style)
 def list_books(request):
