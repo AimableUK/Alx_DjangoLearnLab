3177c148d187f7b91735924c479db1da574493ee
UKOBIZABA Aimable <154277022+AimableTrague@users.noreply.github.com>
2025-09-06 09:26:41 +0200

    FOR CHECK: updated code for checker validation

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path("books/", list_books, name="list_books"),
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
     # Authentication
-    path("login/", login_view, name="login"),
-    path("logout/", logout_view, name="logout"),
+    path("login/", login_view.as_view(template_name="login"), name="login"),
+    path("logout/", logout_view.as_view(template_name="logout"), name="logout"),
     path("register/", register_view, name="register"),
 ]
