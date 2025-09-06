dfb7b83446c089bec7060647a58e92cc141115d8
UKOBIZABA Aimable <154277022+AimableTrague@users.noreply.github.com>
2025-09-06 09:29:59 +0200

    FOR CHECK: register validation

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, LoginView, LogoutView, register
-
+from .views import list_books, LibraryDetailView, LoginView, LogoutView
+from . import views
 urlpatterns = [
     path("books/", list_books, name="list_books"),
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
     # Authentication
     path("login/", LoginView.as_view(template_name="login"), name="login"),
     path("logout/", LogoutView.as_view(template_name="logout"), name="logout"),
-    path("register/", register, name="register"),
+    path("register/", views.register, name="register"),
 ]
