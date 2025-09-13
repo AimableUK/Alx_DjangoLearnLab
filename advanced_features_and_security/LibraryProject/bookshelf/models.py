5eeaa5ebae51dbd31d1303b552906d4f7d506d18
UKOBIZABA Aimable <154277022+AimableTrague@users.noreply.github.com>
2025-09-13 15:09:43 +0200

    Added Perission handling

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -53,3 +53,22 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can view post"),
+            ("can_create", "Can create post"),
+            ("can_edit", "Can edit post"),
+            ("can_delete", "Can delete post"),
+        ]
+        
+
+    def __str__(self):
+        return self.title
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import permission_required
+from .models import Post
 
-# Create your views here.
+
+# View posts
+@permission_required('models.can_view', raise_exception=True)
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, "posts/post_list.html", {"posts": posts})
+
+
+
+# Create post
+@permission_required('models.can_create', raise_exception=True)
+def post_create(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        content = request.POST.get("content")
+        Post.objects.create(title=title, content=content, author=request.user)
+        return redirect("post_list")
+    return render(request, "posts/post_form.html")
+
+
+# Edit post
+@permission_required('models.can_edit', raise_exception=True)
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        post.title = request.POST.get("title")
+        post.content = request.POST.get("content")
+        post.save()
+        return redirect("post_list")
+    return render(request, "posts/post_form.html", {"post": post})
+
+
+# Delete post
+@permission_required('models.can_delete', raise_exception=True)
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect("post_list")
