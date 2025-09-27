0b325a03854845f18d624b8be7c0f2bcd4f0ebb5
UKOBIZABA Aimable <154277022+AimableTrague@users.noreply.github.com>
2025-09-27 13:38:02 +0200

    Added the generics views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,8 +4,24 @@ from .serializers import BookSerializer
 from .models import Book
 
 
-class BookList(generics.ListAPIView):
+class ListView(generics.ListAPIView):
     serializer_class = BookSerializer
     queryset = Book.objects.all()
 
-# class BookDetailView(generics.De)
+
+class CreateView(generics.CreateAPIView):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
+    lookup_field= 'pk'
+    
+
+class UpdateView(generics.UpdateAPIView):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
+    lookup_field= 'pk'
+
+
+class DeleteView(generics.DestroyAPIView):
+    serializer_class = BookSerializer
+    lookup_field= 'pk'
+    
