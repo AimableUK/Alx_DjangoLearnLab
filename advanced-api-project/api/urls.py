814f1a9d46dde037551e684a22d77cf1e3d9deb1
UKOBIZABA Aimable <154277022+AimableTrague@users.noreply.github.com>
2025-09-27 13:57:55 +0200

    feat: Added Permission classes on my views

---
+++ advanced-api-project/api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from .views import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+urlpatterns = [
+    path('/book/', ListView.as_view()),
+    path('/books/<int:pk>/', DetailView.as_view()),
+    path('/books/<int:pk>/', CreateView.as_view()),
+    path('/books/<int:pk>/', UpdateView.as_view()),
+    path('/books/<int:pk>/', DeleteView.as_view()),   
+]
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from rest_framework import generics
 from .serializers import BookSerializer
 from .models import Book
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 
 class ListView(generics.ListAPIView):
     serializer_class = BookSerializer
     queryset = Book.objects.all()
-    
+    permission_classes = [AllowAny]
+
 
 class DetailView(generics.RetrieveAPIView):
     serializer_class = BookSerializer
@@ -19,15 +21,18 @@ class CreateView(generics.CreateAPIView):
     serializer_class = BookSerializer
     queryset = Book.objects.all()
     lookup_field= 'pk'
+    permission_classes = [IsAuthenticated]
     
 
 class UpdateView(generics.UpdateAPIView):
     serializer_class = BookSerializer
     queryset = Book.objects.all()
     lookup_field= 'pk'
+    permission_classes = [IsAuthenticated]
     
 
 class DeleteView(generics.DestroyAPIView):
     serializer_class = BookSerializer
     lookup_field= 'pk'
+    permission_classes = [IsAuthenticated]
     
