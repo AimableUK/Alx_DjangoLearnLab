53390a4db563c1363a05d65f6943e7beff00050f
UKOBIZABA Aimable <154277022+AimableTrague@users.noreply.github.com>
2025-09-27 14:30:25 +0200

    feat: Added searching ordering and filterign functionality

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,11 +3,15 @@ from rest_framework import generics
 from .serializers import BookSerializer
 from .models import Book
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 class ListView(generics.ListAPIView):
     serializer_class = BookSerializer
     queryset = Book.objects.all()
     permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['title', 'author']
+    Ordering_fields = ['title', 'publication_year']
 
 
 class DetailView(generics.RetrieveAPIView):
@@ -19,7 +23,6 @@ class DetailView(generics.RetrieveAPIView):
 class CreateView(generics.CreateAPIView):
     serializer_class = BookSerializer
     queryset = Book.objects.all()
-    lookup_field= 'pk'
     permission_classes = [IsAuthenticated]
     
 
